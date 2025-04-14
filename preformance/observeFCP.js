cbb754668499cc66d83debead8b655fc2da9eeb7
weiwenhao <weiwenhao-szgx@bestpay.com.cn>
2025-04-14 10:52:08 +0800

    first push

---
+++ preformance/observeFCP.js
@@ -0,0 +1,21 @@
+export default function observerFCP() {
+  const entryHandler = (list) => {
+    for(const entry of list.getEntries()) {
+      if (entry.name === 'first-contentful-paint') {
+        observer.disconnect()
+        const json = entry.toJSON()
+        console.log(json);
+        const reportData = {
+          ...json,
+          type: 'performance',
+          subType: entry.name,
+          pageUrl: window.location.href
+        }
+        // 发送数据 todo
+      }
+    }
+  }
+  
+  const observer = new PerformanceObserver(entryHandler)
+  observer.observe({type: 'paint', buffered: true})
+}
